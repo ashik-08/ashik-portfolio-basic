9f63b27313d071ce46f6f9fd4c7c39dbf2ab30d8
ashik-08 <ashikurrahman.8october@gmail.com>
2023-12-25 17:28:36 +0600

    added toggle for navlink

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
 // scroll sections
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
@@ -27,6 +32,10 @@ window.onscroll = () => {
   // sticky header
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 100);
+
   // remove toggle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+
   // animation footer on scroll
 };
